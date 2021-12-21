02edc57f98222bbb630b05e9ec7f6a680681a1c6
UnRealReincarlution <benwhite2413@gmail.com>
2021-12-22 11:27:29 +1300

    Change to discordts

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-const config = {
-    prefix: "!",
-    clientId: "922746501648773180"
-};
-
-export default config;
--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,55 @@
-import { Client, Collection, Message, TextChannel, Intents, GuildManager, GuildMember } from "discord.js";
-import dotenv from "dotenv";
-import {readdir} from "fs";
-import config from "./config";
+import "reflect-metadata";
+import { Intents, Interaction, Message } from "discord.js";
+import { Client } from "discordx";
+import { dirname, importx } from "@discordx/importer";
+import dotenv from "dotenv"
 
-export interface runEvent {
-    message: Message,
-    client: Client,
-    args: string[],
-    dev: boolean,
-    author: GuildMember | undefined
-}
+const client = new Client({
+  simpleCommand: {
+    prefix: "!",
+  },
+  intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ],
+  // If you only want to use global commands only, comment this line
+  botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+  silent: true,
+});
+
+client.once("ready", async () => {
+  // make sure all guilds are in cache
+  await client.guilds.fetch();
+
+  // init all application commands
+  await client.initApplicationCommands({
+    guild: { log: true },
+    global: { log: true },
+  });
+
+  // init permissions; enabled log to see changes
+  await client.initApplicationPermissions(true);
+
+  // uncomment this line to clear all guild commands,
+  // useful when moving to global commands from guild commands
+  //  await client.clearApplicationCommands(
+  //    ...client.guilds.cache.map((g) => g.id)
+  //  );
+
+  console.log("Bot started");
+});
+
+client.on("interactionCreate", (interaction: Interaction) => {
+  client.executeInteraction(interaction);
+});
+
+client.on("messageCreate", (message: Message) => {
+  client.executeCommand(message);
+});
 
 dotenv.config();
 
-// Determine if the environment is a development environment, assign the client and the commands collection.
-const dev = process.env.NODE_ENV === "dev",
-    client = new Client({ intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] }),
-    commands: Collection<string[], (event: runEvent) => any> = new Collection();
+async function run() {
+  await importx(dirname(import.meta.url) + "/{events,commands}/**/*.{ts,js}");
 
-// Search and read the commands directory for all program files within (.js/.ts)
-readdir('./src/commands/', (err, allFiles) => {
-    if (err) console.log(err);
+  client.login(process.env.BOT_TOKEN ?? "");
+}
 
-    // Create an array of all files in the folder that are .js or .ts
-    const files = allFiles.filter(f => f.split('.').pop() === (dev ? 'ts' : 'js'));
-
-    // If there are no commands, exit - otherwise enter them all into the commands collection. 
-    if (files.length <= 0) console.log('No commands found!');
-    else for(let file of files) {
-        const props = require(`./commands/${file}`) as {names: string[], run: (event: runEvent) => any};
-        commands.set(props.names, props.run);
-    }
-});
-
-client.once("ready", () => {
-   console.log(`Logged in as ${client?.user?.tag}`);
-});
-
-if(dev) 
-    client.on('debug', (e) => {
-        console.log(e);
-    });
-
-client.on("message", async message => {
-    // If message not in server, or bot send message, or is a message to ignore - ignore it.
-    if(message.channel.type === "DM" || message.author.bot) return;
-
-    // Log XP Changes when not calling the bot prefix.
-    if(!message.content.startsWith(config.prefix)) return;
-
-    // Determine the message's author
-    const author = await message?.guild?.members.fetch(message.author);
-
-    // Split command arguments
-    const args = message.content.split(/ +/);
-    if(args.length < 1) return;
-
-    // Get the command as a string
-    const command = args.shift()!.toLowerCase().slice(config.prefix.length);
-    const commandFile = commands.find((r, n) => n.includes(command));
-
-    // Run the command file passing in the message, arguments, client and dev environment boolean.
-    if(!commandFile) return;
-    else commandFile({
-        message,
-        args,
-        client,
-        dev,
-        author
-    });
-});
-
-if(process.env.TOKEN) client.login(process.env.TOKEN);
-else { console.error("Create a file called .env and put your bot's token in there."); process.exit(1); }
+run();
