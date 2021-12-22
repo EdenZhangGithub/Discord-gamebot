cd58b04455505303c569e968d55c60c48c97b2f9
UnRealReincarlution <benwhite2413@gmail.com>
2021-12-22 22:12:48 +1300

    SQL Queries

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { dirname, importx } from "@discordx/importer";
 import dotenv from "dotenv"
 import mysql from "mysql"
 
+const connection = mysql.createConnection({
+	host     :  process.env.MYSQL_HOST,
+	user     : 'root',
+	password : 'secret',
+	database : 'bot-db'
+});
+
 const client = new Client({
   simpleCommand: {
     prefix: "!",
@@ -34,15 +41,19 @@ client.once("ready", async () => {
 	//    ...client.guilds.cache.map((g) => g.id)
 	//  );
 
-	const connection = mysql.createConnection({
-		host     : 	process.env.MYSQL_HOST,
-		user     : 'root',
-		password : 'secret',
-		database : 'bot-db'
-	});
-
 	connection.connect();
 
+	connection.query(`CREATE TABLE users (
+		id        uuid,
+		username  varchar(255),
+		money     int,
+		xp        int,
+		level     int
+	)`, function (error, results, fields) {
+		if (error) throw error;
+		console.log('The solution is: ', results[0].solution);
+	});
+	
 	console.log("Bot started");
 });
 
@@ -52,6 +63,29 @@ client.on("interactionCreate", (interaction: Interaction) => {
 
 client.on("messageCreate", (message: Message) => {
   client.executeCommand(message);
+});
+
+client.on("messageCreate", (message: Message) => {
+	// Run a query to check if the user exists currently.
+	connection.query(`SELECT * FROM users WHERE id ==${message.author.id}`, (err, res) => {
+		if(err) { console.log(`ERR: ${err}`); return; }
+		else console.log(res);
+
+		// If they don't exist, create the user and initialize with a default value of 0.
+		if(!res) {
+			console.log("User does not exist!");
+			connection.query(`ALTER TABLE users; ADD ${message.author.id} ${message.author.username} 0 0 0`, (e,r) => {
+				if(e) { console.log(e); return; }
+				console.log(`User ${message.author.username} Created!`);
+			});
+		}
+		// If they DO exist, we increment their xp by x amount, determined by the length of their message.
+		else {
+			
+		}
+	});
+	
+
 });
 
 dotenv.config();
